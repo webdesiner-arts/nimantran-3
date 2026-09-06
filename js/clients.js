7ba8f93307c372b494c0c853ed1b7ad2d03d97f6
webdesiner-arts <bargodemayuresh@gmail.com>
2026-09-06 23:47:25 +0530

    Create clients.js

---
+++ js/clients.js
@@ -0,0 +1,41 @@
+const clients = {
+
+    sharma: {
+        familyName: "Sharma Family",
+
+        arrivalDate: "10 September 2026",
+        arrivalTime: "6:30 PM",
+
+        sthapanaDate: "11 September 2026",
+        sthapanaTime: "10:00 AM",
+
+        aartiTime: "7:30 PM",
+
+        visarjanDate: "14 September 2026",
+        visarjanTime: "5:00 PM",
+
+        address: "Sample Address, Mumbai, Maharashtra",
+
+        mapLink: "https://www.google.com/maps"
+    },
+
+    patil: {
+        familyName: "Patil Family",
+
+        arrivalDate: "12 September 2026",
+        arrivalTime: "7:00 PM",
+
+        sthapanaDate: "13 September 2026",
+        sthapanaTime: "10:00 AM",
+
+        aartiTime: "8:00 PM",
+
+        visarjanDate: "16 September 2026",
+        visarjanTime: "5:30 PM",
+
+        address: "Patil Family Address, Mumbai, Maharashtra",
+
+        mapLink: "https://www.google.com/maps"
+    }
+
+};
